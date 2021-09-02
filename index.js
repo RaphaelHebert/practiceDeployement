5cf9e1c248b26392a65216fa4b4a12afed1a6dfd
Joe Hebert <raphaelhebert18@gmail.com>
2021-09-02 18:59:19 +0200

    deployable to heroku

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const server = express()
+// const dotenv = require('dotenv')
+server.use(express.json())
+server.use(cors())
+
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: "this is cool"})
+})
+
+server.get('/api', (req, res) => {
+    res.status(200).json({ message: "welcome to the api"})
+})
+
+// exemple of process.env. 
+console.log(process.env.USER)
+
+console.log(process.env.PORT)
+
+const port = process.env.PORT || 5000
+server.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
